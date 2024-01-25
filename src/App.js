58aa53a1ca883f905cd6751d284b4584288eb7f7
vedantshah99 <vedantshah@ucsb.edu>
2024-01-24 18:47:06 -0800

    completed header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      {/* Header */}
+      <Header />
+      <Router >
+        <Routes>
+          <Route path='/' element={<h1>Yo</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+
+
+        
+        
+
+        
+
+{/* Tinder Cards */}
+
+{/* Profile Cards */}
+
+{/* Buttons */}
