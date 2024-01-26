663caad3386f37a96d1bb91eb86bb01da98dc2e7
Palvi <psabherwal@ucsb.edu>
2024-01-25 21:23:40 -0800

    adding profile-creation

---
+++ profile-creation/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React from 'react';
+import Header from "./Header";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Create Profile</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ profile-creation/src/Header.js
@@ -0,0 +1,14 @@
+// src/Header.js
+
+import React from 'react'
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="Header">
+            <h1>Blindly</h1>
+        </div>
+    )
+}
+
+export default Header
