9f3f9e304e0ecc4a046a0464cdd25c0d923b856d
Nirmit Ashar <13338417+nirmitashar@users.noreply.github.com>
2024-01-26 23:50:47 -0800

    adding firebase setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ import Header from './Header';
 import Cards from './Cards';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+
+import firebase from './firebase';
+
+// Sign in with Google
+const provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth().signInWithPopup(provider)
+  .then((result) => {
+    var token = result.credential.accessToken;
+    var user = result.user;
+    // ...
+  }).catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+    // ...
+  });
+
 function App() {
   return (
     <div className="App">
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDyYp8IKY4NraCSLo993odTsRYtnNihnyE",
+    authDomain: "dating-test-ea3a9.firebaseapp.com",
+    projectId: "dating-test-ea3a9",
+    storageBucket: "dating-test-ea3a9.appspot.com",
+    messagingSenderId: "579062097080",
+    appId: "1:579062097080:web:6719555e550589ec50004d",
+    measurementId: "G-M157LVLGMD"
+  };
+// Initialize Firebase
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
