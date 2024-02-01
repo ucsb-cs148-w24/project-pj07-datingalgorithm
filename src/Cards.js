6fa8cf0c2a88291fbb19986fc86c707da858e066
vedantshah99 <vedantshah@ucsb.edu>
2024-01-31 23:41:44 -0800

    implemented firebase for profiles

--- src/Cards.js
+++ src/Cards.js
@@ -1,27 +1,21 @@
 import { Card } from "@material-ui/core";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import ProfileCard from "./ProfileCard";
 import "./ProfileCard.css";
 import TinderCard from "react-tinder-card";
 import "./Cards.css";
+import db from "./firebase";
 
 //import "./Cards.css";
 
 function Cards() {
-    const [people, setPeople] = useState([
-        {
-            name: "Tony Stark",
-            hobby: "Flying high and fast cars",
-            image: require("./res/tonyStark.jpg"),
-            score: "90%"
-        },
-        {
-            name: "Bruce Wayne",
-            hobby: "Fighting crime, being Batman",
-            image: require("./res/bruceWayne.jpeg"),
-            score: "88%"
-        },
-    ]);
+    const [people, setPeople] = useState([]);
+
+    useEffect(() => {
+        db.collection("users").onSnapshot((snapshot) => (
+            setPeople(snapshot.docs.map((doc) => doc.data()))
+        ))
+    }, [])
 
     return (
         <div className="cards">
@@ -33,7 +27,7 @@ function Cards() {
                 >
                     <div 
                     className="card"
-                    style = {{backgroundImage: `url(${person.image})`}}
+                    style = {{backgroundImage: `url(${person.picUrl})`}}
                     >   
                     </div>
 
@@ -43,9 +37,9 @@ function Cards() {
                         <h3 style={{fontSize: 50}}>
                             {person.name}
                         </h3>
-                        <p>{person.hobby}</p>
+                        <p>{person.tagline}</p>
                         <p style={{fontSize: 150, position: "absolute", bottom: 40, left: 260}}>
-                            {person.score}
+                            {90}
                         </p>
                     </div>
                     
