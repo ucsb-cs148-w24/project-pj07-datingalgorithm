387f5ee9bd6c02e6fa25d23b3b8e3de76db39245
Xinyan Zhao <xinyan_zhao@ucsb.edu>
2024-02-01 03:50:42 -0800

    first chat

---
+++ Chats.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Chats.css";
+import Chat from "./Chat";
+
+function Chats() {
+    return (
+        <div className="chats">
+            <Chat
+            name="Mark"
+            message="Yo Whats up"
+            timestamp="40 seconds ago"
+            profilePic="..."
+            />
+            <Chat
+            name="Sarah"
+            message="Ola"
+            timestamp="55 minutes ago"
+            profilePic="..."
+            />
+            <Chat
+            name="Ellen"
+            message="Hi!"
+            timestamp="3 days ago"
+            profilePic="..."
+            />
+        </div>
+    );
+}
+
+export default Chats;
