bf80f9b909ef00eb9c05f4484ad1ad2b02708e24
Palvi <psabherwal@ucsb.edu>
2024-02-01 12:06:37 -0800

    updating profile-creation

--- profile-creation/src/Body.js
+++ profile-creation/src/Body.js
@@ -3,24 +3,38 @@
 import React from 'react';
 import './Body.css';
 
+var checks = document.querySelectorAll(".check");
+var max = 2;
+for (var i = 0; i < checks.length; i++)
+    checks[i].onclick = selectiveCheck;
+function selectiveCheck (event) {
+    var checkedChecks = document.querySelectorAll(".check:checked");
+    if (checkedChecks.length >= max + 1)
+        return false;
+}
+
 function Body() {
     return (
         <div className="Body">
             <form action="/action_page.php">
-                <p>First Name</p>
+                <br></br>
+                <p><b>First Name</b></p>
                 <div className="input-group">
                     <input type="text" id="fname" name="firstname" placeholder="First Name"/>
                 </div>
-                <p>Last Name</p>
+                <br></br>
+                <p><b>Last Name</b></p>
                 <div className="input-group">
                     <input type="text" id="lname" name="lastname" placeholder="Last Name"/>
                 </div>
-                <p>Birthday</p>
+                <br></br>
+                <p><b>Birthday</b></p>
                 <div className="input-group">
                     <input type="date" id="bday" /> 
-                    
+                    <br></br>   
                 </div>
-                <p>Gender</p>
+                <br></br>
+                <p><b>Gender</b></p>
                 <select id="gender" name="gender">
                     <option value="" disabled selected>Select</option>
                     <option value="man">Man</option>
@@ -42,8 +56,9 @@ function Body() {
                     <option value="trans">Trans</option>
                     <option value="other">Other</option>
                 </select>
-
-                <p>Interested in</p>
+                <br></br>
+                <br></br>
+                <p><b>Interested in</b></p>
                 <div>
                     <select id="sexual_ori" name="sexual_ori">
                         <option value="" disabled selected>Select</option>
@@ -52,23 +67,45 @@ function Body() {
                         <option value="everyone">Everyone</option>
                     </select>
                 </div>
-
-                <p>Tell me about yourself</p>
+                <br></br>
+                <p><b>Tell me about yourself</b></p>
                 <div className="input-group">
                     <input type="text" id="bio" name="" placeholder="What would you like your potential matches to know about you?"/>
                 </div>
-                    
-
-                <p>What do you enjoy in your spare time? Please select your top 2 options</p>
-                <label><input type="checkbox" class="check" />Reading</label> <p></p>
-                <label><input type="checkbox" class="check" />Watching to Movies</label> <p></p>
-                <label><input type="checkbox" class="check" />Listening to Music</label> <p></p>
-                <label><input type="checkbox" class="check" />Playing Sports</label> <p></p>
-                <label><input type="checkbox" class="check" />Cooking</label> <p></p>
-                <label><input type="checkbox" class="check" />Traveling</label> <p></p>
-                <label><input type="checkbox" class="check" />Gaming</label> <p></p>
-                <label><input type="checkbox" class="check" />Arts and Crafts</label>
-                
+                <br></br>    
+                <p><b>What do you enjoy in your spare time? Please select your top three choices.</b></p>
+                <div class='checkboxes-container'>
+                    <label><input type="checkbox" class="check" />Reading</label> <p></p>
+                    <label><input type="checkbox" class="check" />Watching to Movies</label> <p></p>
+                    <label><input type="checkbox" class="check" />Listening to Music</label> <p></p>
+                    <label><input type="checkbox" class="check" />Playing Sports</label> <p></p>
+                    <label><input type="checkbox" class="check" />Cooking</label> <p></p>
+                    <label><input type="checkbox" class="check" />Traveling</label> <p></p>
+                    <label><input type="checkbox" class="check" />Gaming</label> <p></p>
+                    <label><input type="checkbox" class="check" />Arts and Crafts</label>
+                </div>
+                <br></br>
+                <p><b>What traits best describe you? Please select your top three choices.</b></p>
+                <div class='checkboxes-container'>
+                    <label><input type="checkbox" class="check" />Adventurous</label> <p></p>
+                    <label><input type="checkbox" class="check" />Ambitious</label> <p></p>
+                    <label><input type="checkbox" class="check" />Easygoing</label> <p></p>
+                    <label><input type="checkbox" class="check" />Empathetic</label> <p></p>
+                    <label><input type="checkbox" class="check" />Optimistic</label> <p></p>
+                    <label><input type="checkbox" class="check" />Romantic</label> <p></p>
+                    <label><input type="checkbox" class="check" />Open-minded</label> <p></p>
+                    <label><input type="checkbox" class="check" />Family-oriented</label>
+                </div>
+                <br></br>
+                <p><b>What are your love languages? Please select your top two choices.</b></p>
+                <div class='checkboxes-container'>
+                    <label><input type="checkbox" class="check" />Acts of service</label> <p></p>
+                    <label><input type="checkbox" class="check" />Physical touch</label> <p></p>
+                    <label><input type="checkbox" class="check" />Receiving gifts</label> <p></p>
+                    <label><input type="checkbox" class="check" />Quality time</label> <p></p>
+                    <label><input type="checkbox" class="check" />Words of affirmation</label> <p></p>
+                </div>
+                <br></br>
                 <div>
                     <input type="submit" value="Submit"></input>
                 </div>
