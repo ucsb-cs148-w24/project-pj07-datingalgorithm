e7da19a21bf6dd08fbcc3a8af8e8443d6a04c5ac
vedantshah99 <vedantshah@ucsb.edu>
2024-02-06 13:04:16 -0800

    mvp branch

--- src/Cards.js
+++ src/Cards.js
@@ -3,22 +3,66 @@ import TinderCard from "react-tinder-card";
 import "./Cards.css";
 import { db, auth, signInAnonymously } from './firebase';
 import Header from './Header';
-import {collection, onSnapshot, query, where, getDocs, doc, getDoc} from "firebase/firestore";
+import {collection, onSnapshot, query, where, getDocs, doc, getDoc, addDoc} from "firebase/firestore";
+import { getAuth, onAuthStateChanged} from 'firebase/auth';
 
 const Cards = () =>{
     const [people, setPeople] = useState([]);
 
-    const q = query(collection(db, "users"));
+    const profilesQuery = query(collection(db, "users"));
+    const querySnapshot =  getDocs(profilesQuery);
 
-    const querySnapshot =  getDocs(q);
-
-    useEffect(() => onSnapshot(q, (querySnapshot) => {
+    useEffect(() => onSnapshot(profilesQuery, (querySnapshot) => {
         setPeople(querySnapshot.docs.map(doc => doc.data()))
     }
     ), [db]);
 
-    // print the data
-    console.log(people);
+    const user = getAuth().currentUser;
+
+    const [userChats, setUserChats] = useState([])
+
+    console.log(user.email);
+
+    const usersQuery = query(collection(db, 'chats'), where('users', 'array-contains', user?.email))
+
+    
+
+    useEffect(() => onSnapshot(usersQuery, snapshot=>{
+        setUserChats(snapshot.docs)
+      })
+    , [db]);
+
+    const chatAlreadyExists = (recEmail) =>{
+        return(
+        !!userChats.find(
+            (chat)=>
+            chat.data().users.find((user) => user === recEmail)?.length > 0
+        )
+        )
+    }
+
+
+    const onSwipe = async (direction, personEmail) => {
+        
+        if (direction === "right"){
+            // check if current chat already exists
+            const exists = chatAlreadyExists(personEmail);
+            if (exists){
+                console.log("Chat already exists");
+                return;
+            }
+            
+            const docRef = await addDoc(collection(db, "chats"), {
+                users: [user?.email, personEmail]
+                
+            });
+            console.log("You swiped: " + direction + " to " + personEmail + "from" + user?.email);
+
+        }
+        else if (direction === "left"){
+            console.log("swiped left");
+        }
+    }
 
     return (
         <div className="cards">
@@ -27,7 +71,8 @@ const Cards = () =>{
                 <TinderCard
                     className="swipe"
                     key={person.name}
-                    preventSwipe={["up"]}
+                    preventSwipe={["up", "down"]}
+                    onSwipe={(dir) => onSwipe(dir, person.email)}
                 >
                     <div 
                     className="card"
